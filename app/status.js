34a2ca2188fdea38ae5f511a2a6a4251aab6a6df
Sylvain Faucherand <syl.faucherand@gmail.com>
2019-02-14 15:34:13 +0000

    Increase tests coverage, remove uneeded deps

--- app/status.js
+++ app/status.js
@@ -1,29 +1,11 @@
 const { version } = require('../package.json');
-const redis = require('ioredis');
-const { REDIS_HOST, REDIS_PORT, REDIS_PASSWORD } = process.env;
 
 const get = (req, res) => {
-  if (REDIS_HOST !== undefined) {
-    const client = redis.createClient(REDIS_PORT, {
-      password: REDIS_PASSWORD,
-      host: REDIS_HOST,
-      port: REDIS_PORT,
-      tls: true
-    });
-    client.ping((err, result) =>
-      res.json({
-        api: 'ok',
-        version,
-        redis: result || err
-      })
-    );
-  } else {
-    res.json({
-      api: 'ok',
-      version,
-      env: process.env
-    });
-  }
+  res.json({
+    api: 'ok',
+    version,
+    env: process.env
+  });
 };
 
 module.exports = get;
