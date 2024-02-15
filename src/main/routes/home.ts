b12ba3ba26161b03ef3fbdc075742a08de06b0d1
Tyler-35 <115563258+Tyler-35@users.noreply.github.com>
2024-02-15 15:28:06 +0000

    Update home.ts

--- src/main/routes/home.ts
+++ src/main/routes/home.ts
@@ -15,7 +15,7 @@ export default function (app: Application): void {
       return res.render('home', { recipes });
     } catch (err) {
       logger.error(err.stack);
-      res.status(500).end();;
+      res.status(500).end();
     }
   });
 }
