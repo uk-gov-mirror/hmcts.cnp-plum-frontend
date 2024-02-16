17f49a0f0c7cbe7cab2a3c962e1fa1698b0b06a9
Tyler-35 <115563258+Tyler-35@users.noreply.github.com>
2024-02-16 09:56:25 +0000

    Update health.ts

--- src/main/routes/health.ts
+++ src/main/routes/health.ts
@@ -1,12 +1,24 @@
 import { Application } from 'express';
 
+import config from 'config';
+
+import axios from 'axios';
+
 const healthcheck = require('@hmcts/nodejs-healthcheck');
+
+const recipesUrl = config.get('backendUrl');
 
 export default function (app: Application): void {
   const healthCheckConfig = {
     checks: {
-      // TODO: replace this sample check with proper checks for your application
-      sampleCheck: healthcheck.raw(() => healthcheck.up()),
+      backendCheck: healthcheck.raw(async () => {
+        try {
+          const response = await axios.get(`${recipesUrl}/health/readiness`);
+          return response.status === 200 ? healthcheck.up() : healthcheck.down();
+        } catch (error) {
+          return healthcheck.down();
+        }
+      }),
     },
   };
 
